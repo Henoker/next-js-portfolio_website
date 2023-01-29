1f021e3e60c9911b8eb12408a9556afbf73d5a78
Henoker <hennybany@gmail.com>
2023-01-29 21:28:06 +0300

    feat: Head tags updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,11 @@ import { Home } from "../components/Home/Home";
 export default function HomePage() {
   return (
     <>
+    <Head>
+        <title>Henock Beyene Tesfatsion</title>
+        <meta name="description" content="Software Developer Web Developer" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
     <Home/>
    
     </>
